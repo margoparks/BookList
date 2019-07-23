89de1088d4f4868a22b57fb755fa1a81286472be
margop <margo.parks94@gmail.com>
2019-07-23 13:24:40 -0500

    Configured Database to hold list of books even if deleted from reading list

--- main.py
+++ main.py
@@ -12,22 +12,40 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     author= db.Column(db.String(120))
+    completed = db.Column(db.Boolean, default = False)
 
     def __init__(self, title, author):
         self.title =  title
         self.author = author
-
-books= []
-
+        self.completed = False
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
     if request.method == 'POST':
         book = request.form['book_title']
-        books.append(book)
+        author = request.form['book_author']
+        new_book = Book(book, author)
+        db.session.add(new_book)
+        db.session.commit()
 
-    return render_template('bookForm.html', title="BookList", books=books)
+    books = Book.query.filter_by(completed = False).all()
+    completed_books = Book.query.filter_by(completed = True).all()
+    return render_template('bookForm.html', 
+    title="BookList", books=books, completed_books = completed_books)
 
+
+@app.route('/remove-book', methods=['POST'])
+def remove_book():
+    book_id = int(request.form['book-id'])
+    book = Book.query.get(book_id)
+    book.completed = True
+    db.session.add(book)
+    db.session.commit()
+
+    return redirect('/')
+
+    
 
     
 if __name__ == '__main__': 
